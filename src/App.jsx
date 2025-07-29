ed559d8cb53df71bcd7e04586f1421a1b12494aa
vermaAbhay02 <141313919+vermaAbhay02@users.noreply.github.com>
2025-07-29 15:33:24 +0530

    added copy button

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,80 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 import { getDatabase, ref, set } from "firebase/database";
 import { app, getData, writeData } from "./firebase";
 function App() {
+  let key = window.location.pathname;
 
-let key=window.location.pathname
+  let [text, setText] = useState("");
 
-let [text,setText]=useState("")
+  let [isDataFetched, setIsDataFetched] = useState(false);
 
-let [isDataFetched,setIsDataFetched]=useState(false)
+  let [isTyping, setIsTyping] = useState(false);
 
-let [isTyping,setIsTyping]=useState(false)
+  useEffect(() => {
+    async function readData() {
+      let data = await getData(key);
 
-useEffect(()=>
-  {
-     async function readData()
-    {
-
-      let data= await getData(key)
-      
-      setIsDataFetched(true)
-      console.log("Data Fetched",data)
-      if(isTyping)
-        return 
-      setText(data)
-    //  navigator.clipboard.writeText(data)
+      setIsDataFetched(true);
+      console.log("Data Fetched", data);
+      if (isTyping) return;
+      setText(data);
+      //
     }
-   let interval= setInterval(()=>
-    {
-      readData()
-    },5000)
-    return ()=>
-      {
-        clearInterval(interval)
+    let interval = setInterval(() => {
+      readData();
+    }, 5000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isTyping]);
 
-      } 
+  useEffect(() => {
+    console.log("Text", text);
 
-  },[isTyping])
+    let timmer = setTimeout(
+      () => {
+        let write = async () => {
+          await writeData(key, text);
+          setIsTyping(false);
+        };
+        write();
+      },
 
+      1000
+    );
 
-useEffect(()=>
-  {
-    
-   
-    console.log("Text" ,text)
-   
-    
-    let timmer=setTimeout(()=>
-    {
-      let write= async ()=>
-      {
+    return () => {
+      clearTimeout(timmer);
+    };
+  }, [text]);
 
-
-          await writeData(key,text)
-          setIsTyping(false)
-
-      }
-      write()
-    
-    }
-      
-      ,1000)
-
-
-      return ()=>
-      {
-        clearTimeout(timmer)
-        
-      }
-  },[text])
-
-  
-
-// setInterval(fetchContent,5000);
+  // setInterval(fetchContent,5000);
   return (
     <>
-      <div>    
-        <textarea disabled={!isDataFetched} rows={"35"} cols={"170"} value={text} onChange={(e)=>{setText(e.target.value)
-setIsTyping(true)
-
-        }}>
-          
-        </textarea>
+      <div>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText(text);
+            }}
+          >
+            {" "}
+            Copy
+          </button>
+        </div>
+        <textarea
+          disabled={!isDataFetched}
+          rows={"35"}
+          cols={"170"}
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+            setIsTyping(true);
+          }}
+        ></textarea>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
